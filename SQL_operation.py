b500f0b33d33dc1dffa2ed76f1460de0bfb94a7e
nissaka <maigechuang115@163.com>
2020-10-15 18:01:16 +0800

    add bank back function

--- SQL_operation.py
+++ SQL_operation.py
@@ -16,6 +16,7 @@ vls_bank_url = f"{vls_bank['dbtype']}+{vls_bank['driver']}://{vls_bank['user']}:
 
 # vls_back_url = f"postgresql://{vls_back['user']}:{vls_back['password']}@{vls_back['host']}:{vls_back['port']}/{vls_back['database']}"
 
+
 def login_function(username, password):
     data = postgresql_handle(vls_back_url).list(User_data)
     name_boolen = False
@@ -162,31 +163,104 @@ def operation_log_list(page, per_page, name=None, date_start=None, date_end=None
 # login_function('xingezhe', 'gezhexinlian')
 
 # use violas bank database
-def depositOrBorrowData(data):
+
+
+def depositOrBorrowData2Dict(data):
+    # {
+    # "id":data.id,
+    # "product_id":data.product_id,
+    # "product_name":data.product_name,
+    # "logo":data.logo,
+    # "minimum_amount":data.minimum_amount,
+    # "max_limit":data.max_limit,
+    # "pledge_rate":float(data.pledge_rate),
+    # "description":data.description,
+    # "intor":data.intor,
+    # "question":data.question,
+    # "currency":data.currency,
+    # "rate":float(data.rate),
+    # "rate_desc":data.rate_desc,
+    # "status":data.status
+    # }
+    # {
+    #     "product_id": 10004,
+    #     "product_name": "data.product_name",
+    #     "logo": "data.logo",
+    #     "minimum_amount": "data.minimum_amount",
+    #     "max_limit": "data.max_limit",
+    #     "pledge_rate": 99,
+    #     "description": "data.description",
+    #     "intor": "data.intor",
+    #     "question": "data.question",
+    #     "currency": "data.currency",
+    #     "rate": 11,
+    #     "rate_desc": "data.rate_desc",
+    #     "status": true
+    # }
     return {
-    'id':data.id,
-    'product_id':data.product_id,
-    'product_name':data.product_name,
-    'logo':data.logo,
-    'minimum_amount':data.minimum_amount,
-    'max_limit':data.max_limit,
-    'pledge_rate':float(data.pledge_rate),
-    'description':data.description,
-    'intor':data.intor,
-    'question':data.question,
-    'currency':data.currency,
-    'rate':float(data.rate),
-    'rate_desc':data.rate_desc,
-    'status':data.status
+        'id': data.id,
+        'product_id': data.product_id,
+        'product_name': data.product_name,
+        'logo': data.logo,
+        'minimum_amount': data.minimum_amount,
+        'max_limit': data.max_limit,
+        'pledge_rate': float(data.pledge_rate),
+        'description': data.description,
+        'intor': data.intor,
+        'question': data.question,
+        'currency': data.currency,
+        'rate': float(data.rate),
+        'rate_desc': data.rate_desc,
+        'status': data.status
     }
+
+
+def dict2Table(table, data):
+    for i in data:
+        print(i, data[i])
+
 
 def get_bank_data(type):
     bank_table = ViolasBankBorrowProduct
     if type == 'deposit':
         bank_table = ViolasBankDepositProduct
     data = postgresql_handle(vls_back_url).list(bank_table)
-    result=[]
+    result = []
     for i in data:
-        result.append(depositOrBorrowData(i))
+        result.append(depositOrBorrowData2Dict(i))
+    # result=alchemy2json_many(data)
     return result
 
+
+def bank_data_exit(type, product_name, product_id):
+    bank_table = ViolasBankBorrowProduct
+    if type == 'deposit':
+        bank_table = ViolasBankDepositProduct
+    data = postgresql_handle(vls_back_url).list(bank_table)
+    result = 0
+    for i in data:
+        if i.product_name == product_name or i.product_id == product_id:
+            result = 1
+    return result
+
+
+def edit_bank_data(type, database, data):
+    bank_table = ViolasBankBorrowProduct
+    if database == 'deposit':
+        bank_table = ViolasBankDepositProduct
+    result = 0
+    if type == 'add':
+        # todo
+        dict2Table(bank_table, data)
+        # add_data=ViolasBankDepositProduct()
+        # result=postgresql_handle(bank_table).add()
+    elif type == 'status':
+        print(data)
+        postgresql_handle(vls_back_url).update(
+            bank_table, (bank_table.id == data['id']), {bank_table.status: data['status']})
+        #     postgresql_handle(vls_back_url).update(
+        # User_data, (User_data.name == name), {User_data.status: status})
+        result = 1
+    elif type=='edit':
+        print(data)
+    return result
--- app_service_bank.py
+++ app_service_bank.py
@@ -1,31 +1,24 @@
 from flask_restful import reqparse, abort, Resource, request
-from SQL_operation import get_bank_data
+from SQL_operation import get_bank_data,edit_bank_data
+import json
 
-# parser = reqparse.RequestParser()
-# parser.add_argument('name', required=True, help='Username cannot be blank')
-# parser.add_argument('ip', required=True, help='ip cannot be blank')
-# parser.add_argument('time', required=True, help='time cannot be blank')
-# parser.add_argument('address', required=True, help='address cannot be blank')
-# parser.add_argument('browser', required=True, help='browser cannot be blank')
+parser = reqparse.RequestParser()
+parser.add_argument('type', required=True, help='Operation type cannot be blank')
+parser.add_argument('database', required=True, help='Database cannot be blank')
+parser.add_argument('data', required=True, help='Data cannot be blank')
 
 
 class Bank(Resource):
-    # def post(self):
-    #     __args__ = parser.parse_args()
-    #     __temp__ = login_log_addone(__args__.name, __args__.ip, __args__.time,
-    #                                 __args__.address, __args__.browser)
-    #     if __temp__ > 0:
-    #         return {'status': 'ok'}, 201
-    #     else:
-    #         return {'status': 'error'}, 501
+    def post(self):
+        __args__ = parser.parse_args()
+        __temp__=edit_bank_data(__args__.type,__args__.database,eval(__args__.data))
+        __temp__=1
+        if __temp__ > 0:
+            return {'status': 'ok'}, 201
+        else:
+            return {'status': 'error'}, 501
 
     def get(self):
-        # page = int(request.args.get('page'))
-        # per_page = int(request.args.get('per_page'))
-        # name = request.args.get('name')
-        # date_start = request.args.get('date_start')
-        # date_end = request.args.get('date_end')
-        # __temp__ = login_log_list(page, per_page, name, date_start, date_end)
         type=request.args.get('type')
         __temp__=get_bank_data(type)
         return __temp__
